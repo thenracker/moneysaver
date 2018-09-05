947936e135585c7eecf022a8ad3c1aa4ff59d241
Petr Weissar <weissar.petr@gmail.com>
2018-09-05 21:51:52 +0200

    Init

---
+++ app/src/main/java/cz/weissar/moneysaver/MainActivity.kt
@@ -0,0 +1,16 @@
+package cz.weissar.moneysaver
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            
+        }
+    }
+}
