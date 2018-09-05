792e633d236cfe9c7da874082c5e460e882ad6e3
Petr Weissar <weissar.petr@gmail.com>
2018-09-05 23:34:20 +0200

    Toast a spouštění aktivity na onclick listenera

--- app/src/main/java/cz/weissar/moneysaver/MainActivity.kt
+++ app/src/main/java/cz/weissar/moneysaver/MainActivity.kt
@@ -1,7 +1,11 @@
 package cz.weissar.moneysaver
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +14,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState != null) {
-            
+
+            //sayHello("")
         }
+    }
+
+
+    fun sayHello(view: View) {
+
+        val showCountTextView = view as TextView
+
+        val toast = Toast.makeText(this, showCountTextView.text, Toast.LENGTH_SHORT)
+
+        toast.show()
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        startActivity(intent)
     }
 }
