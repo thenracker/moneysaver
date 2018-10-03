b9e8ae20c108e0617400b4e810a67c0102579e01
Petr Weissar <weissar.petr@gmail.com>
2018-10-03 22:49:49 +0200

    Vše přehozeno do fragmentu

--- app/src/main/java/cz/weissar/moneysaver/ui/MainActivity.kt
+++ app/src/main/java/cz/weissar/moneysaver/ui/MainActivity.kt
@@ -7,9 +7,6 @@ import cz.weissar.moneysaver.ui.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var recycler: RecyclerView
-    //private lateinit var items: MutableList<Item>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,74 +14,6 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commitAllowingStateLoss()
         }
-
-        //recycler = recyclerView
-        //helloTextView.text = "Klikni na mě"
-
-        //initItems()
     }
 
-    /*
-    private fun initItems() {
-
-        items = ItemDao.getAll()
-        val adapter = Adapter()
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-
-    }
-
-    fun sayHello(view: View) {
-
-        val showCountTextView = view as TextView
-
-        val toast = Toast.makeText(this, showCountTextView.text, Toast.LENGTH_SHORT)
-
-        toast.show()
-
-        val i = Item().apply { name = "randál" }
-
-        ItemDao.createOrUpdate(i)
-
-        items.add(i)
-
-        recyclerView.adapter.notifyDataSetChanged()
-    }
-    */
-
-    /*
-    inner class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            var view = LayoutInflater.from(this@MainActivity).inflate(R.layout.row_simple, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun getItemCount(): Int {
-            return items.size
-        }
-
-        override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
-            holder.textView.text = items[position].name
-        }
-
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener, View.OnLongClickListener {
-
-            val textView: TextView = view.findViewById(R.id.textView)
-
-            init {
-                view.setOnLongClickListener(this)
-            }
-
-            override fun onClick(p0: View?) {
-            }
-
-            override fun onLongClick(p0: View?): Boolean {
-                items.removeAt(layoutPosition).delete()
-                notifyDataSetChanged()
-                return true
-            }
-        }
-    }
-    */
 }
--- app/src/main/java/cz/weissar/moneysaver/ui/fragments/MainFragment.kt
+++ app/src/main/java/cz/weissar/moneysaver/ui/fragments/MainFragment.kt
@@ -1,13 +1,24 @@
 package cz.weissar.moneysaver.ui.fragments
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import cz.weissar.moneysaver.R
+import cz.weissar.moneysaver.db.Item
+import cz.weissar.moneysaver.db.ItemDao
 import cz.weissar.moneysaver.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragmenet_main.*
 
 class MainFragment : BaseFragment() {
 
-    companion object { //statická metoda
+    private lateinit var items: MutableList<Item>
+
+    companion object { //static methods
+
         fun newInstance(): MainFragment {
             return MainFragment()
         }
@@ -19,6 +30,50 @@ class MainFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //TODO naše logika <3
+        initItems()
     }
+
+    private fun initItems() {
+
+        items = ItemDao.getAll()
+        val adapter = Adapter()
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+
+    }
+
+    inner class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            var view = LayoutInflater.from(this@MainFragment.context).inflate(R.layout.row_simple, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return items.size
+        }
+
+        override fun onBindViewHolder(holder: Adapter.ViewHolder, position: Int) {
+            holder.textView.text = items[position].name
+        }
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener, View.OnLongClickListener {
+
+            val textView: TextView = view.findViewById(R.id.textView)
+
+            init {
+                view.setOnLongClickListener(this)
+            }
+
+            override fun onClick(p0: View?) {
+            }
+
+            override fun onLongClick(p0: View?): Boolean {
+                items.removeAt(layoutPosition).delete()
+                notifyDataSetChanged()
+                return true
+            }
+        }
+    }
+
 }
