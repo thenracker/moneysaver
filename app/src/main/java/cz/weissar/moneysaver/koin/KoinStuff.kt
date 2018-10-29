b2f3bd895c0516fc69c505a27df9aef044a611fa
Petr Weissar <weissar.petr@gmail.com>
2018-10-29 21:08:22 +0100

    URL na to jak to používat

--- app/src/main/java/cz/weissar/moneysaver/koin/KoinStuff.kt
+++ app/src/main/java/cz/weissar/moneysaver/koin/KoinStuff.kt
@@ -6,6 +6,10 @@ interface HelloRepo { // co jsem pochopil, tak interface není úplně třeba ..
     // ledaže bychom použil nějaký baseRepository jakoby.. to by dávalo smysl?
 
     fun giveHello(): String
+
+
+    // https://insert-koin.io/docs/1.0/getting-started/kotlin/
+    // Reálný využití KOIN s data class
 }
 
 class HelloRepoImpl : HelloRepo {
